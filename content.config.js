c019035300bd6fd8dcb1ecb13c53dc0fb5f4a69e
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-12 22:21:46 +0200

    x

--- content.config.js
+++ content.config.js
@@ -6,12 +6,19 @@ export default defineContentConfig({
             type: 'page',
             source: 'articles/*.md',
             schema: z.object({
+                title: z.string(),
+                description: z.string().optional(),
                 date: z.string()
             })
         }),
         project: defineCollection({
             type: 'page',
             source: 'projects/*.md'
+            schema: z.object({
+                title: z.string(),
+                description: z.string().optional(),
+                date: z.string()
+            })
         })
     }
 })
