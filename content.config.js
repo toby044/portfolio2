f53ba906471ced7ae2d9f00b5f51e3b031202691
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-12 22:11:22 +0200

    x

--- content.config.js
+++ content.config.js
@@ -1,14 +1,18 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
     collections: {
         article: defineCollection({
             type: 'page',
             source: 'articles/*.md'
-        }),
-        project: defineCollection({
-            type: 'page',
-            source: 'projects/*.md'
-        })
-    }
+            schema: z.object({
+                title: z.string(),
+                description: z.string(),
+                date: z.string()
+            }),
+            project: defineCollection({
+                type: 'page',
+                source: 'projects/*.md'
+            })
+        }
 })
