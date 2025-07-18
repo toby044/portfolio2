60491d464be41bdd2c934cd573e791c9aba27035
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-18 10:52:20 +0200

    fix: idk

--- content.config.js
+++ content.config.js
@@ -2,19 +2,6 @@ import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
     collections: {
-        pages: defineCollection({
-            type: 'page',
-            source: [
-                '**',
-                '!articles/*.md',
-                '!projects/*.md'
-            ],
-            schema: z.object({
-                title: z.string(),
-                description: z.string().optional(),
-            }),
-        }),
-
         article: defineCollection({
             type: 'page',
             source: 'articles/*.md',
