f6a8ec696ed769b4a747727d3058a25c677e4878
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-21 20:14:50 +0200

    ggg

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,38 +1,10 @@
 <template>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    <div class="max-w-[65ch] mx-auto">
-        <AppHeader />
-        <div class="min-h-[80vh] ">
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     <div class="bg-[#D3D5DF] min-h-screen">
         <div class="ml-[var(--dynamic-site-header-height)] p-16">
             <AsideMenu />
             <AppHeader />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
             <slot />
             <!-- <AppFooter /> -->
         </div>
-<<<<<<< Updated upstream
-        <AppFooter />
-=======
->>>>>>> Stashed changes
-=======
-            <slot />
-            <!-- <AppFooter /> -->
-        </div>
->>>>>>> Stashed changes
-=======
-            <slot />
-            <!-- <AppFooter /> -->
-        </div>
->>>>>>> Stashed changes
     </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,4 @@
 <template>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    <div class="grid grid-cols-1 gap-8 pb-4">
-        <p class="text-sm text-gray-500 italic">
-            !Disclaimer: this project is new and is still under development, any
-            questions can be directed to my mail in the footer.
-        </p>
-        <div>
-            <p class="text-sm">
-                Developer based in Vejle, Denmark, excited about crafting great
-                user experiences and functioning frontends. I'm into
-                TailwindCSS, creative problem-solving, table tennis, and making
-                good food for the soul.
-            </p>
-        </div>
-        <Skills />
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     <div class="mt-16 color-[rgb(var(--color-theme))]">
         <HeadIcon class="lg:text-[25vw] !mb-0" />
         <span class="text-[7rem] leading-[80%] font-bold">
@@ -31,9 +9,8 @@
             creatively
         </span>
         <!-- <Skills />
->>>>>>> Stashed changes
         <Socials />
-        <Recommendations />
+        <Recommendations /> -->
         <!-- Primary technologies? -->
     </div>
 </template>
