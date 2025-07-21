4699563851b0cff49475bb5c2e340888da772458
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-21 20:16:02 +0200

    go

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,12 +8,49 @@
             <ProblemsIcon class="px-1.5 inline-block !text-[10vw]" />problems
             creatively
         </span>
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+    <div class="grid grid-cols-1 gap-8 pb-4">
+        <p class="text-sm text-gray-500 italic">
+            !Disclaimer: this project is new and is still under development, any
+            questions can be directed to my mail in the footer.
+        </p>
+        <div>
+            <p class="text-sm">
+                Developer based in Vejle, Denmark, excited about crafting great
+                user experiences and functioning frontends. I'm into
+                TailwindCSS, creative problem-solving, table tennis, and making
+                good food for the soul.
+            </p>
+        </div>
+        <Skills />
+=======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+    <div class="mt-16 color-[rgb(var(--color-theme))]">
+        <HeadIcon class="lg:text-[25vw] !mb-0" />
+        <span class="text-[7rem] leading-[80%] font-bold">
+            Passionate about building
+            <BlocksIcon class="px-1.5 inline-block !text-[10vw]" />user-friendly
+            frontends and solving
+            <ProblemsIcon class="px-1.5 inline-block !text-[10vw]" />problems
+            creatively
+        </span>
         <!-- <Skills />
+>>>>>>> Stashed changes
         <Socials />
-        <Recommendations /> -->
+        <Recommendations />
         <!-- Primary technologies? -->
     </div>
 </template>
+<script setup>
+import HeadIcon from "~/assets/svgs/head.svg";
+import BlocksIcon from "~/assets/svgs/blocks.svg";
+import ProblemsIcon from "~/assets/svgs/problems.svg";
+</script>
 <script setup>
 import HeadIcon from "~/assets/svgs/head.svg";
 import BlocksIcon from "~/assets/svgs/blocks.svg";
