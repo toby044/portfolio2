651ee0b175ae571ee8a076810eb55fb0dcfa6692
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-22 09:09:53 +0200

    fix: single

--- content.config.js
+++ content.config.js
@@ -2,7 +2,7 @@ import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
     collections: {
-        articles: defineCollection({
+        article: defineCollection({
             schema: z.object({
                 title: z.string(),
                 description: z.string(),
