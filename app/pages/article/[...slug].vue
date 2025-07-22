0aa55c1f23ae10aaf5fc29e5c7d99efd7bebc197
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-22 09:38:10 +0200

    fix: wil it work

--- app/pages/article/[...slug].vue
+++ app/pages/article/[...slug].vue
@@ -1,13 +1,13 @@
 <template>
     <NuxtLayout name="article-layout">
-        <span class="text-xl font-semibold"></span>
+        <!-- <span class="text-xl font-semibold"></span>
         <h1
             v-if="page?.title"
             class="text-9xl -tracking-[3.5px] leading-[80%] font-semibold mb-10"
         >
             {{ page.title }}
         </h1>
-        <pre>{{ page }}</pre>
+        <pre>{{ page }}</pre> -->2
         <ContentRenderer
             v-if="page && page.body"
             :value="page"
@@ -16,10 +16,11 @@
 </template>
 <script setup>
 const route = useRoute();
-const { data: page } = await useAsyncData(route.path, () => {
-    return queryCollection("article").path(route.path).first();
-});
 
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection('article').path(route.path).first()
+})
+console.log(page)
 // if (!page.value) {
 //     throw createError({
 //         statusCode: 404,
