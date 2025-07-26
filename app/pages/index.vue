1d42f068ea729e82897f9f61f73e8453479bf1c4
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-26 14:28:43 +0200

    fix: sizing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,14 +2,14 @@
     <div class="mt-16 color-[var(--theme-main)]">
         <HeadIcon class="text-[60vw] sm:text-[40vw] lg:text-[25vw] !mb-0" />
         <span
-            class="text-[3rem] sm:text-[5rem] leading-[90%] lg:text-[8rem] lg:leading-[80%] lg:-tracking-[3.5px] font-bold block mb-12"
+            class="text-[3rem] sm:text-[5rem] md:text-[6.5rem] leading-[90%] lg:text-[8rem] lg:leading-[80%] lg:-tracking-[3.5px] font-bold block mb-12"
         >
             Passionate about building
             <BlocksIcon
-                class="hidden lg:inline-block px-1.5 align-middle h-[1em] w-auto"
+                class="hidden xl:inline-block px-1.5 align-middle h-[1em] w-auto"
             />user-friendly frontends and solving
             <ProblemsIcon
-                class="hidden lg:inline-block px-1.5 align-middle h-[1em] w-auto"
+                class="hidden xl:inline-block px-1.5 align-middle h-[1em] w-auto"
             />problems creatively
         </span>
         <AppHeader class="lg:hidden" />
