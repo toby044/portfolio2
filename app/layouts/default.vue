cd51dc8080a4ef44d7bdbe3f512739485f9d0349
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-26 14:25:52 +0200

    fix: spacing

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="min-h-screen">
         <div class="lg:ml-[var(--dynamic-site-header-height)] p-6 pb-16 lg:p-16">
             <AsideMenu />
-            <AppHeader class="hidden lg:flex" />
+            <AppHeader class="hidden lg:flex mb-[25vh]" />
             <slot />
             <!-- <AppFooter /> -->
         </div>
