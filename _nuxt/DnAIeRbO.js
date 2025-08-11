f04f1104094639e51b9afc0f3db6dc7a799f33ac
toby044 <62332472+toby044@users.noreply.github.com>
2025-08-11 20:54:25 +0000

    Deploying to gh-pages from @ toby044/portfolio2@318a864efbddd113a3346867299e7492c2f54181 🚀

--- _nuxt/DnAIeRbO.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,d as c,e as r,h as e,W as o}from"./zMDCEPyF.js";const a={},n={class:"grid grid-cols-3 gap-4"},i={class:"grow"},_={class:"grow"},d={class:"grow"};function l(s,g){return r(),c("div",n,[e("div",i,[o(s.$slots,"left")]),e("div",_,[o(s.$slots,"center")]),e("div",d,[o(s.$slots,"right")])])}const h=Object.assign(t(a,[["render",l]]),{__name:"Pictures"});export{h as default};
