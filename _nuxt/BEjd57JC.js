941c209cef82c1af41cdc7a29ee7ad552f5071c9
toby044 <62332472+toby044@users.noreply.github.com>
2025-08-28 21:47:10 +0000

    Deploying to gh-pages from @ toby044/portfolio2@a46bc56b42c95618f66b90849e1cac7aa04bfbbd 🚀

---
+++ _nuxt/BEjd57JC.js
@@ -0,0 +1,1 @@
+import{_ as s,d as a,e as r,X as t}from"./CQH2QE99.js";const n={};function o(e,c){return r(),a("table",null,[t(e.$slots,"default")])}const _=Object.assign(s(n,[["render",o]]),{__name:"ProseTable"});export{_ as default};
