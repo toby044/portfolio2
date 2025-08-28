a46bc56b42c95618f66b90849e1cac7aa04bfbbd
toby044 <tobiasnolesen@gmail.com>
2025-08-28 23:45:55 +0200

    fix: testing more stuff

--- app/components/ThemeSelector.vue
+++ app/components/ThemeSelector.vue
@@ -1,50 +1,43 @@
 <template>
+  <button
+    aria-label="Select a theme"
+    class="fixed z-50 right-0 bottom-4 pl-2 h-10 pr-1 rounded-l-full flex cursor-pointer items-center justify-center bg-[var(--theme-main)] text-[var(--theme-background)] group"
+    @click="toggleOpen"
+  >
+    <Icon
+      v-if="isOpen"
+      name="material-symbols:close"
+      class="size-5 lg:size-6 group-hover:-translate-x-1 transition-transform duration-200"
+    />
+    <Icon
+      v-if="!isOpen"
+      name="material-symbols:settings"
+      class="size-5 lg:size-6 group-hover:-translate-x-1 transition-transform duration-200"
+    />
+  </button>
   <div
-    class="fixed z-50 right-0 bottom-4"
-    :class="{
-      '': isOpen,
-    }"
+    class="fixed rounded-l-[25px] z-10 flex flex-row-reverse bottom-4 right-10 gap-1"
   >
     <button
-      aria-label="Select a theme"
-      class="relative z-20 pl-2 h-10 pr-1 rounded-l-full flex cursor-pointer items-center justify-center bg-[var(--theme-main)] text-[var(--theme-background)] group"
-      @click="toggleOpen"
+      v-for="(item, index) in themes"
+      :key="item.value"
+      class="capitalize cursor-pointer h-10 w-10 flex items-center justify-center ease-out rounded-full transition-all"
+      :class="{
+        'opacity-0 translate-x-2': !isOpen,
+      }"
+      :style="{
+        backgroundColor: `rgb(${item.main})`,
+        transitionDelay: isOpen
+          ? `${index * 0.05}s`
+          : `${(themes.length - index - 1) * 0.05}s`,
+      }"
+      @click="setTheme(item.name)"
     >
-      <Icon
-        v-if="isOpen"
-        name="material-symbols:close"
-        class="size-5 lg:size-6 group-hover:-translate-x-1 transition-transform duration-200"
-      />
-      <Icon
-        v-if="!isOpen"
-        name="material-symbols:settings"
-        class="size-5 lg:size-6 group-hover:-translate-x-1 transition-transform duration-200"
-      />
+      <span
+        class="size-4 inline-block rounded-full"
+        :style="{ backgroundColor: `rgb(${item.background})` }"
+      ></span>
     </button>
-    <div
-      class="absolute rounded-l-[25px] z-10 flex flex-row-reverse bottom-0 right-10 gap-1"
-    >
-      <button
-        v-for="(item, index) in themes"
-        :key="item.value"
-        class="capitalize cursor-pointer h-10 w-10 flex items-center justify-center ease-out rounded-full transition-all"
-        :class="{
-          'opacity-0 translate-x-2': !isOpen,
-        }"
-        :style="{
-          backgroundColor: `rgb(${item.main})`,
-          transitionDelay: isOpen
-            ? `${index * 0.05}s`
-            : `${(themes.length - index - 1) * 0.05}s`,
-        }"
-        @click="setTheme(item.name)"
-      >
-        <span
-          class="size-4 inline-block rounded-full"
-          :style="{ backgroundColor: `rgb(${item.background})` }"
-        ></span>
-      </button>
-    </div>
   </div>
 </template>
 <script setup>
