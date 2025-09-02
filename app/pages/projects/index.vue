4864a3de09cf183e9e9bf596aa2186f63fd8afe6
toby044 <tobiasnolesen@gmail.com>
2025-09-02 23:59:54 +0200

    fix: projects

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -1,116 +1,122 @@
 <template>
-    <div class="overflow-visible font-semibold">
-        <div class="grid-cols-8 gap-4 mt-16 hidden lg:grid">
-            <p class="col-span-2">Project</p>
-            <p>Date</p>
-            <p>Employer</p>
-            <p class="col-span-2 pl-12">Role</p>
-        </div>
-        <SectionSpacer class="hidden lg:block" />
-        <ul
-            class="mt-[calc(var(--dynamic-site-header-height)+64px)] lg:mt-0"
-            @mouseleave="activeIdx = null"
-        >
-            <li
-                v-for="(item, idx) in projects"
-                :key="idx"
-                class="lg:mb-8 grid lg:grid-cols-8 lg:gap-4 relative"
-                @mouseenter="handleEnter(idx)"
-            >
-                <a
-                    :href="item.url"
-                    target="_blank"
-                    class="col-span-2 h-fit w-fit flex items-center gap-1 underline group mb-4 lg:mb-0"
-                >
-                    <span class="">
-                        {{ item.title }}
-                    </span>
-                    <Icon
-                        name="mdi:arrow-top-right"
-                        size="24px"
-                        class="inline-block ml-2 transition-transform duration-300 ease-[var(--easing-ease-slow)] group-hover:-translate-y-0.5 group-hover:translate-x-0.5"
-                    />
-                </a>
-                <div class="grid relative lg:col-span-6 lg:grid-cols-6 gap-4">
-                    <Transition name="fade-out">
-                        <NuxtImg
-                            v-if="activeIdx === idx"
-                            :key="activeIdx"
-                            :src="item.img.src"
-                            :alt="item.img.alt"
-                            loading="eager"
-                            class="absolute z-[1] h-40 w-auto rounded-md shadow-md pointer-events-none hidden lg:block"
-                            :style="{ ...activeStyle }"
-                        />
-                    </Transition>
-                    <NuxtImg
-                        :src="item.img.src"
-                        :alt="item.img.alt"
-                        loading="eager"
-                        class="w-full block lg:hidden"
-                        :style="{ ...activeStyle }"
-                    />
-                    <p>{{ item.year }}</p>
-                    <p>{{ item.employer }}</p>
-                    <p class="lg:pl-12 lg:col-span-4">{{ item.role }}</p>
-                    <SectionSpacer
-                        class="lg:hidden"
-                        :class="{ hidden: idx === projects.length - 1 }"
-                    />
-                </div>
-            </li>
-        </ul>
+  <div
+    class="font-semibold grid grid-cols-1 gap-24 mt-[calc(var(--dynamic-site-header-height,64px)+64px)] lg:mt-0"
+  >
+    <div v-for="item in projects" class="grid grid-cols-2">
+      <div
+        class="h-fit sticky top-[var(--dynamic-site-header-height,64px)] left-0 pr-24"
+      >
+        <NuxtLink :to="item.url" class="flex items-center group w-fit">
+          <h3 class="text-lg font-bold group-hover:underline">
+            {{ item.title }}
+          </h3>
+          <Icon
+            name="mdi:arrow-top-right"
+            size="20px"
+            class="inline-block ml-2 transition-transform duration-300 ease-[var(--easing-ease-slow)] group-hover:-translate-y-0.5 group-hover:translate-x-0.5"
+          />
+        </NuxtLink>
+        <span
+          class="text-sm max-w-3/4 s-rich-text"
+          v-html="item.description"
+        ></span>
+      </div>
+      <div v-if="item.images" class="min-h-screen">
+        <NuxtImg
+          v-for="image in item.images"
+          :src="image.src"
+          :alt="image.alt"
+          loading="eager"
+          class="c-project-image w-full block"
+        />
+      </div>
     </div>
+  </div>
 </template>
 <script setup>
-const activeIdx = ref(null);
-const activeStyle = ref({});
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
-function handleEnter(idx) {
-    if (window.innerWidth > 1024) {
-        activeStyle.value = {
-            left: Math.floor(Math.random() * 66) - 15 + "%",
-            top: Math.floor(Math.random() * 101) - 50 + "%",
-        };
-        activeIdx.value = idx;
-    }
-}
 const projects = [
-    {
-        title: "Hovedstadens Beredskab",
-        url: "https://www.hbr.dk/",
-        year: "2025",
-        employer: "Limbo.Works",
-        img: { src: "/media/img/hbr.png", alt: "Hovedstadens Beredskab" },
-        role: "Frontend Developer, in charge of implementing the design system. I've acted as one of the key developers in this project, building the fundamentals for a bunch of the APIs, taken decisions on the structure etc. This is composed of a set of components, utilities, and guidelines to ensure consistency across the website. The website is built using Nuxt3, UnoCSS and a bunch of custom packages from Limbo.Works.",
-    },
+  {
+    title: "Hovedstadens Beredskab",
+    description:
+      "<p>Frontend Developer, in charge of implementing the design system.</p> <p>I've acted as one of the key developers in this project, building the fundamentals for a bunch of the APIs, taking decisions on the structure etc.</p> <p>This is composed of a set of components, utilities, and guidelines to ensure consistency across the website.</p> <p>The website is built using Nuxt4, UnoCSS and a bunch of internal packages from Limbo.Works.</p>",
+    url: "https://www.egetberedskab.dk/",
+    year: "2025",
+    employer: "Limbo.Works",
+    images: [
+      { src: "/media/img/hbr/hbr.png", alt: "Hovedstadens Beredskab" },
+      // { src: "/media/img/hbr/hbr2.png", alt: "Hovedstadens Beredskab" },
+      // { src: "/media/img/hbr/hbr3.png", alt: "Hovedstadens Beredskab" },
+      { src: "/media/img/hbr/hbr4.png", alt: "Hovedstadens Beredskab" },
+      { src: "/media/img/hbr/hbr5.png", alt: "Hovedstadens Beredskab" },
+    ],
+  },
+  {
+    title: "Limbo Citi",
+    description:
+      "<p>A modular frontend engine for Danish Municipalities.</p><p>I've acted as a frontend developer in this project, working on components, page types, ensuring the frontend is pixel-perfect to the design, user-friendly and accessible.</p><p>I have also implemented various subsites, which could be stuff like local school websites in the municipalities.</p><p>A challenge here was making sure everything was consistent and worked across ALL solutions, as a simple change could have an impact on everything.</p><p>I have worked on the websites for Randers, Svendborg, Vejle, Viborg, Nyborg, Roskilde, Odsherred and Aalborg municipalities, aswell as the subsites.</p>",
+    url: "https://www.limbociti.dk/",
+    year: "2024",
+    employer: "Limbo.Works",
+    images: [
+      {
+        src: "/media/img/citi/viborg.png",
+        alt: "Viborg Kommune",
+      },
+      {
+        src: "/media/img/citi/nyborg.png",
+        alt: "Nyborg Kommune",
+      },
+      {
+        src: "/media/img/citi/vejle.png",
+        alt: "Vejle Kommune",
+      },
+      {
+        src: "/media/img/citi/svendborg.png",
+        alt: "Svendborg Kommune",
+      },
+      {
+        src: "/media/img/citi/randers.png",
+        alt: "Randers Kommune",
+      },
+      {
+        src: "/media/img/citi/roskilde.png",
+        alt: "Roskilde Kommune",
+      },
+    ],
+  },
 ];
-const citiProjects = [
-    {
-        title: "Viborg Kommune",
-        url: "https://www.viborg.dk/",
-        year: "2024",
-        employer: "Limbo.Works",
-        img: { src: "/media/img/viborg.png", alt: "Viborg Kommune" },
-        role: "Frontend Developer, in charge of implementing the design system. This is composed of a set of components, utilities, and guidelines to ensure consistency across the website. The website is built using Nuxt3, UnoCSS and a bunch of custom packages from Limbo.Works.",
-    },
-    {
-        title: "Nyborg Kommune",
-        url: "https://www.nyborg.dk/",
-        year: "2024",
-        employer: "Limbo.Works",
-        img: { src: "/media/img/nyborg.png", alt: "Nyborg Kommune" },
-        role: "Frontend Developer, in charge of implementing the design system. This is composed of a set of components, utilities, and guidelines to ensure consistency across the website. The website is built using Nuxt3, UnoCSS and a bunch of custom packages from Limbo.Works.",
-    },
-];
+
+onMounted(() => {
+  const animateImages = () => {
+    const images = document.querySelectorAll(".c-project-image");
+    images.forEach((img) => {
+      gsap.from(img, {
+        y: 40,
+        opacity: 0.001,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: img,
+          start: "top 90%",
+        },
+      });
+    });
+  };
+
+  animateImages();
+});
 </script>
 <style>
 .fade-out-enter-active,
 .fade-out-leave-active {
-    transition: opacity 1s var(--easing-ease-slow);
+  transition: opacity 1s var(--easing-ease-slow);
 }
 .fade-out-enter-from,
 .fade-out-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
